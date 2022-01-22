e9c45ccd228066b0afe6130f7395f3479a1e6263
Shinya Fujino <shf0811@gmail.com>
2022-01-22 18:29:30 +0900

    Add a test for readDir

--- src/ptree.test.ts
+++ src/ptree.test.ts
@@ -1,5 +1,44 @@
-describe("foo", () => {
-  test("hello", () => {
-    expect("hello").toBe("hello");
+import mockConsole, { RestoreConsole } from "jest-mock-console";
+import mockFs from "mock-fs";
+
+import { readDir } from "./ptree";
+
+describe("readDir", () => {
+  let restoreConsole: RestoreConsole;
+  beforeEach(() => {
+    restoreConsole = mockConsole();
+    mockFs({
+      foo: {
+        "bar.txt": "",
+        baz: {
+          hello: "",
+        },
+        meow: "",
+      },
+    });
+  });
+
+  afterEach(() => {
+    restoreConsole();
+    mockFs.restore();
+  });
+
+  it("returns dirEntries", async () => {
+    const entries = await readDir("foo");
+    const expected = [
+      {
+        name: "bar.txt",
+        isDirectory: false,
+      },
+      {
+        name: "baz",
+        isDirectory: true,
+      },
+      {
+        name: "meow",
+        isDirectory: false,
+      },
+    ];
+    expect(entries).toEqual(expected);
   });
 });
