0a62da36c89632becc3f55c9faad7ca9ebafc61e
Shinya Fujino <shf0811@gmail.com>
2022-01-22 22:38:56 +0900

    Add tests for ptree

--- src/ptree.test.ts
+++ src/ptree.test.ts
@@ -1,12 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import mockConsole, { RestoreConsole } from "jest-mock-console";
 import mockFs from "mock-fs";
 
-import { readDir } from "./ptree";
+import { ptree, readDir } from "./ptree";
 
 describe("readDir", () => {
-  let restoreConsole: RestoreConsole;
   beforeEach(() => {
-    restoreConsole = mockConsole();
     mockFs({
       foo: {
         "bar.txt": "",
@@ -19,7 +18,6 @@ describe("readDir", () => {
   });
 
   afterEach(() => {
-    restoreConsole();
     mockFs.restore();
   });
 
@@ -40,5 +38,62 @@ describe("readDir", () => {
       },
     ];
     expect(entries).toEqual(expected);
+  });
+});
+
+describe("ptree", () => {
+  let restoreConsole: RestoreConsole;
+  beforeEach(() => {
+    restoreConsole = mockConsole();
+    mockFs({
+      foo: {
+        "bar.txt": "",
+        baz: {
+          hello: "",
+        },
+        meow: "",
+      },
+      emptyDir: {},
+    });
+  });
+
+  afterEach(() => {
+    restoreConsole();
+    mockFs.restore();
+  });
+
+  it("prints only name if empty directory given", async () => {
+    await ptree("emptyDir");
+    // @ts-ignore
+    expect(console.log.mock.calls).toEqual([["emptyDir"]]);
+  });
+
+  it("prints tree with emojis", async () => {
+    await ptree("foo");
+    // @ts-ignore
+    expect(console.log.mock.calls).toEqual([
+      ["foo"],
+      ["├── 📄 bar.txt"],
+      ["├── 📁 baz"],
+      ["│   └── 📄 hello"],
+      ["└── 📄 meow"],
+    ]);
+  });
+
+  it("prints nothing if depth < 1", async () => {
+    await ptree("foo", { maxDepth: 0 });
+    // @ts-ignore
+    expect(console.log.mock.calls).toEqual([]);
+  });
+
+  it("prints only direct children if depth = 1", async () => {
+    await ptree("foo", { maxDepth: 1 });
+    // @ts-ignore
+    expect(console.log.mock.calls).toEqual([
+      ["foo"],
+      ["├── 📄 bar.txt"],
+      ["├── 📁 baz"],
+      ["└── 📄 meow"],
+    ]);
   });
 });
